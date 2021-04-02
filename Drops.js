a481f5b46e5f1dc833e4f3e91691ea14f5ce5443
Saanvi4115 <73887007+Saanvi4115@users.noreply.github.com>
2021-04-02 14:49:48 +0530

    Add files via upload

---
+++ Drops.js
@@ -0,0 +1,34 @@
+class Drops {
+    constructor(x, y,r) {
+
+        var options ={
+            restitution:0.4
+        }
+        this.r=r;
+      
+        this.body = Bodies.circle(x, y, this.r,options);       
+        this.color=color(255);
+        World.add(world, this.body);
+
+    }
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        noStroke();
+        fill(this.color)
+        ellipseMode(RADIUS);
+        ellipse(0, 0, this.r,this.r);
+        pop();
+    }
+    update(){
+      if(this.rain.position.y > height){
+          Matter.Body.setPosition(this.rain, {x:RandomSource(0,400), y:(0,400)})
+      }  
+    }
+
+};
